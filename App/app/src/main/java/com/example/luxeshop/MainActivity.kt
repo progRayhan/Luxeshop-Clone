3f5705489a0af7461329eb572f39df0d422f1555
Mohammad Rayhan <rayhanul.goni@upaybd.com>
2024-12-16 12:36:00 +0600

    feat: header design

--- App/app/src/main/java/com/example/luxeshop/MainActivity.kt
+++ App/app/src/main/java/com/example/luxeshop/MainActivity.kt
@@ -4,13 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.luxeshop.ui.theme.LuxeShopTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +38,43 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LuxeShopTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Header()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Header(){
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 30.dp).padding(top = 50.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row (
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            AppLogo(painter = painterResource(R.drawable.clock))
+            Spacer(modifier = Modifier.width(8.dp))
+            Text("LuxeShop",
+                style = MaterialTheme.typography.bodyLarge,
+                fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+            )
+        }
+        Row (
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            AppLogo(painter = painterResource(R.drawable.shopping))
+            Spacer(modifier = Modifier.width(3.dp))
+            AppLogo(painter = painterResource(R.drawable.notification))
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LuxeShopTheme {
-        Greeting("Android")
-    }
+fun AppLogo(painter: Painter){
+    Image(
+        painter = painter,
+        contentDescription = "LuxeShop",
+        modifier = Modifier.size(35.dp)
+    )
 }
